3e9a2a7810814da8a46be51c232cac0f1255b246
TN03 <h.irmler@t-online.de>
2019-11-20 00:35:44 +0100

    Fix  typos in copyright-block

--- classes/SystemCheckService.php
+++ classes/SystemCheckService.php
@@ -5,20 +5,20 @@
  * Copyright 2017 Christoph M. Becker
  * Copyright 2019 Holger Irmler
  *
- * This file is part of Cnc_XH.
+ * This file is part of Maintenance_XH.
  *
- * Cnc_XH is free software: you can redistribute it and/or modify
+ * Maintenance_XH is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Cnc_XH is distributed in the hope that it will be useful,
+ * Maintenance_XH is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Cnc_XH. If not, see <http://www.gnu.org/licenses/>.
+ * along with Maintenance_XH. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Maintenance;
